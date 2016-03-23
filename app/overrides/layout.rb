95d31f358264ae79481ab789276a711968f4c428
NoelDiazMesa <noeldiaz83@gmail.com>
2016-03-23 08:32:17 +0000

    fixes on layoute.rb

--- app/overrides/layout.rb
+++ app/overrides/layout.rb
@@ -1,4 +1,9 @@
 Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
                      :name => 'shadow_layer',
-                     :insert_bottom => "div.container",
+                     :insert_bottom => "div#content",
                      :text => '<div id="cd-shadow-layer"></div>')
+                     
+Deface::Override.new(:virtual_path => 'spree/layouts/spree_application',
+                     :name => 'add_mini_cart',
+                     :insert_bottom => "div#content",
+                     :partial => "spree/shared/minicart")
